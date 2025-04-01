c5ababbdc3a63be68cabad68c9dacbd8c59bad7c
Ahmed22193 <Ahmed221050193@gmail.com>
2025-04-02 01:49:10 +0200

    Update My Website

--- Ahmed.js
+++ Ahmed.js
@@ -1,4 +1,4 @@
-let counters = document.querySelectorAll('.counter_ele h3');
+/* let counters = document.querySelectorAll('.counter_ele h3');
 let Section_ = document.querySelector('.Show_Counter');
 let active = false; // is the counter active ? No
 
@@ -10,8 +10,8 @@ window.onscroll = function(){
         active = true;
     }
 }
-
-function startCount(el){
+ */
+/* function startCount(el){
     let goal = el.dataset.goal;
     let count = setInterval(()=>{
         el.textContent++;
@@ -19,5 +19,19 @@ function startCount(el){
             clearInterval(count)
         }
     },10)
-}
+} */
 
+
+// send to my whatsapp 
+const handleSubmit = () => {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+  
+    const whatsappMessage = `اسم: ${name}\nإيميل: ${email}\nموضوع: ${subject}\nرسالة: ${message}`;
+    const whatsappLink = `https://wa.me/01096825124?text=${encodeURIComponent(whatsappMessage)}`;
+    
+    window.open(whatsappLink, '_blank');
+};
+  
